b463f700ae7b7e427d01d33bf43d3f998d77855a
SK <doublesk0810@gmail.com>
2020-12-16 22:41:52 +0900

    Add Calendar Dialog btn and xaml, cs that is base

---
+++ HighStock/ViewModel/CalendarViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighStock.ViewModel {
+    public class CalendarViewModel : INotifyPropertyChanged {
+        private List<DateTime> selectedDates;
+        public List<DateTime> SelectedDates {
+            get { return selectedDates; }
+            set {
+                selectedDates = value;
+                RaisePropertyChanged("SelectedDates");
+            }
+        }
+        private DateTime selDate;
+        public DateTime SelDate {
+            get { return selDate; }
+            set {
+                selDate = value;
+                Console.WriteLine("Test : " + selDate);
+                RaisePropertyChanged("SelDate");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CalendarViewModel() {
+            SelectedDates = new List<DateTime>();
+            SelectedDates.Add(DateTime.Now.Date);
+            SelectedDates.Add(DateTime.Now.Date.AddDays(1));
+            SelectedDates.Add(DateTime.Now.Date.AddDays(2));
+        }
+
+        private void RaisePropertyChanged(String Name) {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(Name));
+        }
+    }
+}
--- HighStock/ViewModel/MainViewModel.cs
+++ HighStock/ViewModel/MainViewModel.cs
@@ -16,8 +16,7 @@ using System.Diagnostics;
 using HighStock.Type;
 using System.Globalization;
 
-namespace HighStock.ViewModel
-{
+namespace HighStock.ViewModel {
     class MainViewModel : INotifyPropertyChanged {
         DBControl dbcontrol;
         SettingInfo settingInfo = new SettingInfo() { VolumeValue = 100, BullishValue = 20 };
@@ -35,7 +34,7 @@ namespace HighStock.ViewModel
             //StockDates=dbcontrol.GetTotalStockDate( 10000000, 29);
         }
 
-        SettingStockInfo settingStockInfo = new SettingStockInfo("000","0", "0", "0", "0","000", "0", "0", "0"); //세팅값 아직 적용전, dialog와 기본 구조만 만들어놓음
+        SettingStockInfo settingStockInfo = new SettingStockInfo("000", "0", "0", "0", "0", "000", "0", "0", "0"); //세팅값 아직 적용전, dialog와 기본 구조만 만들어놓음
 
         private String txtDateData; //날짜입력 text창, calendar로 대체 예정
         public String TxtDateData {
@@ -77,10 +76,10 @@ namespace HighStock.ViewModel
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged(string propertyName) { //WPF의 핵심, 변수가 변경되었을 경우 이벤트를 발생해 이 함수에 들어오게 된다. 
-            if(PropertyChanged != null) {
+            if (PropertyChanged != null) {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
-            switch(propertyName) {
+            switch (propertyName) {
                 case "SelectStockDate":
                     TxtDateData = selectStockDate.StrDate;
                     Console.WriteLine("selcetDate : " + selectStockDate.StrDate);
@@ -102,9 +101,9 @@ namespace HighStock.ViewModel
         public ICommand SettingCommand { //ICommand 는 기본적으로 버튼 클릭시, 메소드를 실행해준다.
             get { return (this.settingCommand) ?? (this.settingCommand = new DelegateCommand(Setting)); }
         }
-        
+
         private void Setting() { //세팅 dialog인데 미완성
-            Console.WriteLine("1. Setting : "+settingInfo.BullishValue +", "+ settingInfo.VolumeValue);
+            Console.WriteLine("1. Setting : " + settingInfo.BullishValue + ", " + settingInfo.VolumeValue);
             var settingDialog = new SettingDialog();
             var settingViewModel = new SettingViewModel(settingInfo);
             settingDialog.DataContext = settingViewModel;
@@ -123,32 +122,32 @@ namespace HighStock.ViewModel
                 MessageBox.Show("please input Date");
                 return;
             } else {
-                if(txtDateData.Contains("-") && txtDateData[4] == '-' && txtDateData[7] == '-') {
+                if (txtDateData.Contains("-") && txtDateData[4] == '-' && txtDateData[7] == '-') {
                     tmpTxtDate = txtDateData;
-                }else if(txtDateData.Length == 8) {
+                } else if (txtDateData.Length == 8) {
                     int chknum = 0;
                     bool isnum = int.TryParse(txtDateData, out chknum);
-                    if(isnum) {
+                    if (isnum) {
                         tmpTxtDate = txtDateData;
                         tmpTxtDate = tmpTxtDate.Insert(6, "-");
                         tmpTxtDate = tmpTxtDate.Insert(4, "-");
                     }
-                } else {     
+                } else {
                     MessageBox.Show("날짜 형식이 잘못되었습니다.");
                     return;
                 }
             }
             foreach (var i in stockDates) {
-                if(i.StrDate == tmpTxtDate) {
+                if (i.StrDate == tmpTxtDate) {
                     SelectStockDate = i;
                     return;
                 }
             }
 
             DateTime dt = DateTime.ParseExact(tmpTxtDate, "yyyy-MM-dd", null);
-            if (dbcontrol.GetYear() != dt.Year)  {
+            if (dbcontrol.GetYear() != dt.Year) {
                 dbcontrol = new DBControl(dt.Year);
-            } 
+            }
             StockDate sd = dbcontrol.GetSelectStockDate(dt, 10000000, 29);
             if (sd == null) {
                 MessageBox.Show("DB가 존재하지 않습니다. 날짜를 확인하세요.");
@@ -158,88 +157,92 @@ namespace HighStock.ViewModel
 
 
         }
+        private DateTime cDate = DateTime.Now;
+        private ICommand calendarCommand; // 달력
+        public ICommand CalendarCommand {
+            get { return (this.calendarCommand) ?? (this.calendarCommand = new DelegateCommand(OepnCalendar)); }
+        }
+        private void OepnCalendar() {
+            var calendarDialog = new CalendarDialog();
+            var calendarViewModel = new CalendarViewModel();//
+            calendarDialog.DataContext = calendarViewModel;
+            calendarDialog.ShowDialog();
 
-    
-        
-    }
-    #region DelegateCommand Class 
-    public class DelegateCommand : ICommand //하기 클래스의 경우, ICommand를 사용하기 위한 함수, 복붙하면 쉽게 가져오는 함수
-    {
-
-        private readonly Func<bool> canExecute;
-        private readonly Action execute;
-
-        /// <summary>
-        /// Initializes a new instance of the DelegateCommand class.
-        /// </summary>
-        /// <param name="execute">indicate an execute function</param>
-        public DelegateCommand(Action execute) : this(execute, null)
-        {
         }
 
-        /// <summary>
-        /// Initializes a new instance of the DelegateCommand class.
-        /// </summary>
-        /// <param name="execute">execute function </param>
-        /// <param name="canExecute">can execute function</param>
-        public DelegateCommand(Action execute, Func<bool> canExecute)
-        {
-            this.execute = execute;
-            this.canExecute = canExecute;
-        }
-        /// <summary>
-        /// can executes event handler
-        /// </summary>
-        public event EventHandler CanExecuteChanged;
 
-        /// <summary>
-        /// implement of icommand can execute method
-        /// </summary>
-        /// <param name="o">parameter by default of icomand interface</param>
-        /// <returns>can execute or not</returns>
-        public bool CanExecute(object o)
+        #region DelegateCommand Class 
+        public class DelegateCommand : ICommand //하기 클래스의 경우, ICommand를 사용하기 위한 함수, 복붙하면 쉽게 가져오는 함수
         {
-            if (this.canExecute == null)
-            {
-                return true;
+
+            private readonly Func<bool> canExecute;
+            private readonly Action execute;
+
+            /// <summary>
+            /// Initializes a new instance of the DelegateCommand class.
+            /// </summary>
+            /// <param name="execute">indicate an execute function</param>
+            public DelegateCommand(Action execute) : this(execute, null) {
             }
-            return this.canExecute();
-        }
 
-        /// <summary>
-        /// implement of icommand interface execute method
-        /// </summary>
-        /// <param name="o">parameter by default of icomand interface</param>
-        public void Execute(object o)
-        {
-            this.execute();
-        }
+            /// <summary>
+            /// Initializes a new instance of the DelegateCommand class.
+            /// </summary>
+            /// <param name="execute">execute function </param>
+            /// <param name="canExecute">can execute function</param>
+            public DelegateCommand(Action execute, Func<bool> canExecute) {
+                this.execute = execute;
+                this.canExecute = canExecute;
+            }
+            /// <summary>
+            /// can executes event handler
+            /// </summary>
+            public event EventHandler CanExecuteChanged;
 
-        /// <summary>
-        /// raise ca excute changed when property changed
-        /// </summary>
-        public void RaiseCanExecuteChanged()
-        {
-            if (this.CanExecuteChanged != null)
-            {
-                this.CanExecuteChanged(this, EventArgs.Empty);
+            /// <summary>
+            /// implement of icommand can execute method
+            /// </summary>
+            /// <param name="o">parameter by default of icomand interface</param>
+            /// <returns>can execute or not</returns>
+            public bool CanExecute(object o) {
+                if (this.canExecute == null) {
+                    return true;
+                }
+                return this.canExecute();
+            }
+
+            /// <summary>
+            /// implement of icommand interface execute method
+            /// </summary>
+            /// <param name="o">parameter by default of icomand interface</param>
+            public void Execute(object o) {
+                this.execute();
+            }
+
+            /// <summary>
+            /// raise ca excute changed when property changed
+            /// </summary>
+            public void RaiseCanExecuteChanged() {
+                if (this.CanExecuteChanged != null) {
+                    this.CanExecuteChanged(this, EventArgs.Empty);
+                }
             }
         }
-    }
-    #endregion
+        #endregion
 
 
-    public static class DispatcherService { //UI 쓰레드와 프로그램 내부 쓰레드 충돌방지를 위한 클래스인데 사용하나?? 모르겠음
-        public static void Invoke(Action action) {
-            Dispatcher dispatchObject = Application.Current != null ? Application.Current.Dispatcher : null;
-            if (dispatchObject == null || dispatchObject.CheckAccess())
-                action();
-            else
-                dispatchObject.Invoke(action);
+        public static class DispatcherService { //UI 쓰레드와 프로그램 내부 쓰레드 충돌방지를 위한 클래스인데 사용하나?? 모르겠음
+            public static void Invoke(Action action) {
+                Dispatcher dispatchObject = Application.Current != null ? Application.Current.Dispatcher : null;
+                if (dispatchObject == null || dispatchObject.CheckAccess())
+                    action();
+                else
+                    dispatchObject.Invoke(action);
+            }
         }
+
+
+
+
     }
-
- 
-
-
 }
